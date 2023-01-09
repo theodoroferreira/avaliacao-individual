fb35dadc95ac5897c3f9030a1b9d68cd392dde80
Theodoro Gaspar Ferreira <theodoro.ferreira.pb@compasso.com.br>
2023-01-09 16:56:13 -0300

    Commit inicial

---
+++ ms-history/src/test/java/br/com/pb/mshistory/MsHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pb.mshistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
